79bf5011d7baa5f9fb701262e1e028373082ceba
Nik Efimov <efimov.nikolai.ig@gmail.com>
2023-01-14 15:18:17 -0500

    Updates

---
+++ precache-manifest.335925878143c33b7462fd64b2e6f58e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6260a5ce3fc72e152db1c8931b18669e",
+    "url": "/todo-application/index.html"
+  },
+  {
+    "revision": "be39aa5e40180a253479",
+    "url": "/todo-application/static/css/main.a49208d8.chunk.css"
+  },
+  {
+    "revision": "a8363c2292966be4bc64",
+    "url": "/todo-application/static/js/2.56eabbb7.chunk.js"
+  },
+  {
+    "revision": "be39aa5e40180a253479",
+    "url": "/todo-application/static/js/main.1228300b.chunk.js"
+  },
+  {
+    "revision": "0136a1df90b65abad18b",
+    "url": "/todo-application/static/js/runtime-main.e89ee6eb.js"
+  }
+]);
